212b14d33b9caaeb96dca0f2afc3cfbaaa2aa1d9
clearchris <clearchristx@gmail.com>
2015-11-03 07:42:03 -0600

    boot animation change
    Change-Id: I67691f94f738c061fbfbe59a3371ffc17a169396

--- device.mk
+++ device.mk
@@ -23,8 +23,7 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay
 # Boot animation and screen size
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
+PAC_BOOTANIMATION_NAME := 720
 PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
